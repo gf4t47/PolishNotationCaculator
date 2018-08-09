627967145cfcc2694f6cf1a2b24407f41654c068
Kern <gf4t47@gmail.com>
2018-08-08 18:17:11 -0700

    start on parser

--- src/interpreter/parser/parser.py
+++ src/interpreter/parser/parser.py
@@ -1,5 +1,15 @@
 from src.interpreter.lexer.token import TokenType, Token
+from src.interpreter.parser.node.binary import BinaryOp
+from src.interpreter.parser.node.factory import FactorNode, Num
+from src.interpreter.parser.node.node import AstNode
 from src.interpreter.parser.token_stream import TokenStream
+
+
+def syntax_error_wrapper(action, *argv):
+    try:
+        return True, action(argv)
+    except SyntaxError as e:
+        return False, e
 
 
 class Parser:
@@ -8,21 +18,74 @@ class Parser:
         self.current_token = self._token_streams.next_token()
 
     @property
-    def current_token(self)-> Token:
+    def current_token(self) -> Token:
         return self._current_token
 
     @current_token.setter
     def current_token(self, value: Token):
         self._current_token = value
 
-    def eat(self, token_type: TokenType)-> bool:
+    def eat(self, token_type: TokenType) -> bool:
         if self.current_token.type == token_type:
             self.current_token = self._token_streams.next_token()
             return True
 
-        raise TypeError(f'unexpected token type {self.current_token.type}, expecting {type}')
+        raise SyntaxError(f'unexpected token type {self.current_token.type}, expecting {type}')
 
-    def term(self):
+    def number(self) -> Num:
+        """
+        number:
+            Token.TokenType == NUMBER
+        :return: Num
+        """
+        node = Num(self.current_token)
+        self.eat(TokenType.NUMBER)
+        return node
+
+    def factor(self) -> FactorNode:
+        """
+        factor:
+            number
+            | LPAREN number RPAREN
+        :return: FactoryNode
+        """
+        if self.current_token.type == TokenType.NUMBER:
+            return self.number()
+        elif self.current_token.type == TokenType.BRACKET and self.current_token.value is True:
+            self.eat(TokenType.BRACKET)
+            node = self.number()
+            assert self.current_token.type == TokenType.BRACKET and self.current_token.value is False
+            self.eat(TokenType.BRACKET)
+            return node
+
+        raise SyntaxError(f'Unexpected factor token {self.current_token}')
+
+    def factor_list(self) -> [FactorNode]:
+        """
+        factor_list:
+            factor factor (factor)*
+        :return: [FactorNode]
+        """
+        factors = [self.factor(), self.factor()]
+        return factors
+
+    def formula(self) -> BinaryOp:
+        """
+        formula:
+            OP factor_list
+            | LPAREN OP factor_list RPAREN
+        :return: BinaryOp
+        """
+        pass
+
+    def term(self) -> AstNode:
+        """
+        term: factor
+            | formula
+            | OP factor (term)+
+            | OP formula (term)+
+        :return AstNode:
+        """
         pass
 
     def parse(self):
@@ -32,4 +95,3 @@ class Parser:
             raise TypeError(f'the token stream is not finished after parsing, last token = {self.current_token}')
 
         return node
-
--- src/interpreter/parser/token_stream.py
+++ src/interpreter/parser/token_stream.py
@@ -5,6 +5,7 @@ from src.interpreter.lexer.token import TokenType, Token
 class TokenStream:
     def __init__(self, lexer: Lexer):
         self.__pos = 0
+        self._peek_pos = 0
         self._stream = []
         cur_token = lexer.next_token()
         while cur_token.type != TokenType.EOF:
@@ -16,4 +17,16 @@ class TokenStream:
     def next_token(self)-> Token:
         token = self._stream[self.__pos]
         self.__pos += 1
+        self.reset_peek()
         return token
+
+    def peek_token(self) -> Token:
+        token = self._stream[self._peek_pos]
+        self._peek_pos += 1
+        return token
+
+    def commit_peek(self):
+        self.__pos = self._peek_pos
+
+    def reset_peek(self):
+        self._peek_pos = self.__pos
