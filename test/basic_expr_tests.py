18e53bb206aedaa834302be343ee20e69a0029fb
Kern <gf4t47@gmail.com>
2018-08-11 19:05:28 -0700

    connect to existed tests.

--- test/basic_expr_tests.py
+++ test/basic_expr_tests.py
@@ -1,6 +1,7 @@
 import pytest
 from tools.expression_builder import unary_generator, binary_generator
-from src.main import stack_calc as evaluate
+from src.main import stack_calc
+from src.main import interpreter_calc
 
 
 left_data = [0, 1, 2, 10, 2]
@@ -9,29 +10,34 @@ right_data = [0, 1, 3, 22]
 
 @pytest.mark.parametrize("expr, expected", unary_generator(set(left_data + right_data)))
 @pytest.mark.parametrize("binary_op", [True, False])
-def test_unary(expr, expected, binary_op):
-    assert expected == evaluate(expr, binary_op)
+@pytest.mark.parametrize("calc", [stack_calc, interpreter_calc])
+def test_unary(expr, expected, binary_op, calc):
+    assert expected == calc(expr, binary_op)
 
 
 @pytest.mark.parametrize("expr, expected", binary_generator(left_data, right_data, '+', None))
 @pytest.mark.parametrize("binary_op", [True, False])
-def test_binary_add(expr, expected, binary_op):
-    assert expected == evaluate(expr, binary_op)
+@pytest.mark.parametrize("calc", [stack_calc, interpreter_calc])
+def test_binary_add(expr, expected, binary_op, calc):
+    assert expected == calc(expr, binary_op)
 
 
 @pytest.mark.parametrize("expr, expected", binary_generator(left_data, right_data, '-', None))
 @pytest.mark.parametrize("binary_op", [True, False])
-def test_binary_sub(expr, expected, binary_op):
-    assert expected == evaluate(expr, binary_op)
+@pytest.mark.parametrize("calc", [stack_calc, interpreter_calc])
+def test_binary_sub(expr, expected, binary_op, calc):
+    assert expected == calc(expr, binary_op)
 
 
 @pytest.mark.parametrize("expr, expected", binary_generator(left_data, right_data, '*', None))
 @pytest.mark.parametrize("binary_op", [True, False])
-def test_binary_mul(expr, expected, binary_op):
-    assert expected == evaluate(expr, binary_op)
+@pytest.mark.parametrize("calc", [stack_calc, interpreter_calc])
+def test_binary_mul(expr, expected, binary_op, calc):
+    assert expected == calc(expr, binary_op)
 
 
 @pytest.mark.parametrize("expr, expected", binary_generator(left_data, right_data, '/', None))
 @pytest.mark.parametrize("binary_op", [True, False])
-def test_binary_div(expr, expected, binary_op):
-    assert expected == evaluate(expr, binary_op)
+@pytest.mark.parametrize("calc", [stack_calc, interpreter_calc])
+def test_binary_div(expr, expected, binary_op, calc):
+    assert expected == calc(expr, binary_op)
--- test/combination_tests.py
+++ test/combination_tests.py
@@ -1,16 +1,19 @@
 import pytest
 from tools.expression_builder import ternary_generator_all_op
-from src.main import stack_calc as evaluate
+from src.main import stack_calc
+from src.main import interpreter_calc
 
 left_data = [0, 1, 10]
 right_data = [1, 343]
 
 
 @pytest.mark.parametrize("expr, expected", ternary_generator_all_op(left_data, right_data, None))
-def test_ternary_binary_op(expr, expected):
-    assert expected == evaluate(expr, True)
+@pytest.mark.parametrize("calc", [stack_calc, interpreter_calc])
+def test_ternary_binary_op(expr, expected, calc):
+    assert expected == calc(expr, True)
 
 
 @pytest.mark.parametrize("expr, expected", ternary_generator_all_op(left_data, right_data, True))
-def test_ternary_free_op(expr, expected):
-    assert expected == evaluate(expr, False)
+@pytest.mark.parametrize("calc", [stack_calc, interpreter_calc])
+def test_ternary_free_op(expr, expected, calc):
+    assert expected == calc(expr, False)
